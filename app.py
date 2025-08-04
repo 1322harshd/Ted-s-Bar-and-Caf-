dd4eb6db395c9e0fc68b46b740cf29cf1fc22490
1322harshd <harsh.nz@outlook.com>
2025-08-04 20:10:39 +1200

    added navigation and footer to 'base.html'

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+#about page route
+@app.route("/about")
+def about_page():
+  return render_template('base.html')
+
+# Run the development server
+if __name__ == '__main__':
+    app.run(debug=True) 
